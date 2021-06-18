8d6e71648046111e9bf0a66199210038fb4c45fc
Fabian <fab.salas@alumnos.duoc.cl>
2021-06-18 01:16:53 -0400

    Funciones en views.py
    Funciones para que las files se puedan visualizar.

--- P3_Web_django/core/backend/views.py
+++ P3_Web_django/core/backend/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def base(request):
+
+    return render(request, 'backend/Base.html')
+
+def signin(request):
+
+    return render(request, 'backend/Signin.html')
+
+def signup(request):
+
+    return render(request, 'backend/Signup.html')
+
+def inicio(request):
+
+    return render(request, 'backend/Inicio.html')
