8c447152914f88f71d367e0f3335f5501e6092ad
Fabian <fab.salas@alumnos.duoc.cl>
2021-06-19 03:38:15 -0400

    Iniciamos a crear el modelo
    En models.py creamos las tablas con sus atributos.

--- P3_Web_django/core/backend/models.py
+++ P3_Web_django/core/backend/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+# Tabla componente y sus atributos
+class Componente(models.Model):
+     id     = models.AutoField(primary_key=True)
+     nombre = models.CharField(max_length=99,)
+# Tabla Fabricante y sus atributos
+class Fabricante(models.Model):
+    id     = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=99,)
+    
+# Tabla Producto y sus atributos
+class Producto(models.Model):
+    id          = models.AutoField(primary_key=True)
+    nombre      = models.CharField(max_length=99,)
+    fabricante  = models.ForeignKey(Fabricante, on_delete=models.CASCADE, null=True, blank=True)
+    componente  = models.ForeignKey(Componente, on_delete=models.CASCADE, null=True, blank=True)
+    precio      = models.IntegerField(default=0)
