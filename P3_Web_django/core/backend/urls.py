2f9c94f7d2d214afe7b89e6585df6a0f73c61d4c
Fabian <fab.salas@alumnos.duoc.cl>
2021-06-19 02:58:42 -0400

    avance en proyecto

--- P3_Web_django/core/backend/urls.py
+++ P3_Web_django/core/backend/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from .views import base, signin, signup, hardware
@@ -7,4 +9,4 @@ urlpatterns = [
     path('signin', signin, name = 'signin'),
     path('signup', signup, name = 'signup'),
     path('hardware', hardware, name = 'hardware')
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
