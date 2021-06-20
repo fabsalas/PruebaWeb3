9260db6d6bcffd14a31e6c0a2882dfc6d95903b8
Fabian <fab.salas@alumnos.duoc.cl>
2021-06-20 03:59:51 -0400

    Se crean las files de los productos
    MB,CPU,GPU,PSU,RAM,HDD,SDD,M2

--- P3_Web_django/core/backend/urls.py
+++ P3_Web_django/core/backend/urls.py
@@ -2,11 +2,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from .views import base, signin, signup, hardware
+from .views import base, cpu, gpu, hdd, m2, mb, psu, ram, sdd, signin, hardware
 
 urlpatterns = [
     path('', base, name = 'base'),
     path('signin', signin, name = 'signin'),
-    path('signup', signup, name = 'signup'),
-    path('hardware', hardware, name = 'hardware')
+    path('hardware', hardware, name = 'hardware'),
+    path('mb', mb, name='mb'),
+    path('cpu', cpu, name='cpu'),
+    path('gpu', gpu, name='gpu'),
+    path('psu', psu, name='psu'),
+    path('ram', ram, name='ram'),
+    path('hdd', hdd, name='hdd'),
+    path('sdd', sdd, name='sdd'),
+    path('m2', m2, name='m2')
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- P3_Web_django/core/backend/views.py
+++ P3_Web_django/core/backend/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+#importamos las clases de los modelos
+from .models import Producto
 
 # Create your views here.
 
 def base(request):
+    #accediendo al objeto que contiene los datos de la base de datos
+    #el metodo all traera todos los datos que estan en la tabla
     contexto = {}
     return render(request, 'base.html', contexto)
 
@@ -10,11 +14,43 @@ def signin(request):
 
     return render(request, 'signin.html',{})
 
-def signup(request):
 
-    return render(request, 'signup.html',{})
 
 def hardware(request):
 
-    return render(request, 'hardware.html')
+    return render(request, 'hardware.html',{})
 
+
+def mb(request):
+
+    return render(request, 'mb.html',{})
+
+
+def cpu(request):
+
+    return render(request, 'cpu.html',{})
+
+
+def gpu(request):
+
+    return render(request, 'gpu.html',{})
+
+def psu(request):
+
+    return render(request, 'psu.html',{})
+
+def ram(request):
+
+    return render(request, 'ram.html',{})
+
+def hdd(request):
+
+    return render(request, 'hdd.html',{})
+
+def sdd(request):
+
+    return render(request, 'sdd.html',{})
+
+def m2(request):
+
+    return render(request, 'm2.html',{})
